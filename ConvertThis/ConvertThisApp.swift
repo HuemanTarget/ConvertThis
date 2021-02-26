3d7f36e68eef7ae8b06b3fa13900a0302b8826fd
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-02-25 19:55:49 -0800

    first commit

---
+++ ConvertThis/ConvertThisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertThisApp.swift
+//  ConvertThis
+//
+//  Created by Joshua Basche on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct ConvertThisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
